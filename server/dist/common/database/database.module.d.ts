3691ec9bdd09b1f1badac6e25b780096b63dda4a
Rayton Lin <raytonlin@gmail.com>
2024-05-29 01:34:00 -0400

    Add validation and logging template

--- server/dist/common/database/database.module.d.ts
+++
@@ -1,4 +0,0 @@
-import { ModelDefinition } from '@nestjs/mongoose';
-export declare class DatabaseModule {
-    static forFeature(models: ModelDefinition[]): import("@nestjs/common").DynamicModule;
-}
--- server/dist/users/dto/create-user.input.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreateUserInput {
-    email: string;
-    password: string;
-}
