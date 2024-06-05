bdc5c5d32d8ce479d35bce8f731586b91d3130af
Rayton Lin <raytonlin@gmail.com>
2024-06-05 01:07:45 -0400

    Apollo client setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,25 @@ import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/materia
 import Auth from "./components/auth/Auth";
 import { RouterProvider } from "react-router-dom";
 import router from "./components/Routes";
+import { ApolloProvider } from "@apollo/client"
+import client from "./constants/apollo-client";
 
+// We use Apollo client state management library for GraphQL queries
 const darkTheme = createTheme({
   palette: {mode: 'dark'}
 })
 
 const App = () => {
-  return <ThemeProvider theme={darkTheme}>
-    <CssBaseline/>
-    <Container>
-      <RouterProvider router = {router}/>
-    </Container>
-  </ThemeProvider>
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      <Container>
+        <RouterProvider router = {router}/>
+      </Container>
+    </ThemeProvider>
+    </ApolloProvider>
+  )
 };
 
 export default App;
