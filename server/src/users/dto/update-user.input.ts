3a62f89f3ceb3520ee7aaa7f5f662d8873cd22bd
Rayton Lin <raytonlin@gmail.com>
2024-06-14 23:32:55 -0400

    Added graphql auth

--- server/src/users/dto/update-user.input.ts
+++ server/src/users/dto/update-user.input.ts
@@ -1,9 +1,5 @@
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, PartialType } from '@nestjs/graphql';
+import { InputType, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateUserInput extends PartialType(CreateUserInput) { 
-  //PartialType extends the CreateUserInput type
-  @Field()
-  _id: string;
-}
+export class UpdateUserInput extends PartialType(CreateUserInput) {}
